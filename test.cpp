651c33c53f0365b58ce142fed8e250d3bdc7caaa
Zhao-Rigel <1287852595@qq.com>
2022-11-10 18:26:56 +0800

    test

---
+++ test.cpp
@@ -0,0 +1,14 @@
+//第一个图形窗口
+#include <graphics.h>
+int main()
+{
+    //初始化为640*480大小
+    initgraph(640, 480);
+    
+    //等待用户按键
+    getch();
+
+    //关闭图形界面
+    closegraph();
+    return 0;
+}
